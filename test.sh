0a963fa0f475a9d0be887d0a65d323f54fa7dfc2
Jerry Ye's Macbook Pro <runan.ye@outlook.com>
2023-03-07 18:30:03 -0800

    Add video files

--- test.sh
+++ test.sh
@@ -11,3 +11,7 @@ make
 
 ./interpolate 131-dance.asf 131_04-dance.amc b e 20 4-b-e.amc
 ./interpolate 131-dance.asf 131_04-dance.amc b q 20 4-b-q.amc
+
+./interpolate 135-martialArts.asf 135_06-martialArts.amc b e 40 v1.amc
+./interpolate 135-martialArts.asf 135_06-martialArts.amc l q 40 v2.amc
+./interpolate 135-martialArts.asf 135_06-martialArts.amc b q 40 v3.amc
